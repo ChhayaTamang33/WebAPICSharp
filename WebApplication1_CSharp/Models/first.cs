adabaa7582f470e8e0c998064652a8b81d067166
ChhayaTamang33 <jagchhaya@gmail.com>
2024-05-27 16:53:18 +0545

    Add project files.

---
+++ WebApplication1_CSharp/Models/first.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1_CSharp.Models
+{
+    public class first
+    {
+        public int Id { get; set; }
+        public string SenderPcName { get; set; }
+        public DateTime JobSentTime { get; set; }
+        public string FileName { get; set; }
+        public string CameraName { get; set; }
+        public string UserEmail { get; set; }
+        public string HUDSettings { get; set; }
+        public string RemotePCName { get; set; }
+    }
+}
